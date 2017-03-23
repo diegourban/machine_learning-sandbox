426df860a58e203faac45ecd5ee0dc1a3f728a56
Diego Leonardo Urban <diego.urban88@gmail.com>
2017-03-22 21:58:53 -0300

    validation pahse

--- classify_searches.py
+++ classify_searches.py
@@ -14,32 +14,68 @@ Ydummies_df = Y_df
 X = Xdummies_df.values
 Y = Ydummies_df.values
 
-# Separating training and test data
-training_percentage = 0.9
+# Separating training, test and validation data
+training_percentage = 0.8
+test_percentage = 0.1
 
 training_size = int(training_percentage * len(Y))
-training_data = X[:training_size]
-training_markings = Y[:training_size]
+test_size = int(test_percentage * len(Y))
+validation_size = len(Y) - training_size - test_size
 
-test_size = len(Y) - training_size
-test_data = X[-test_size:]
-test_markings = Y[-test_size:]
+training_data = X[0:training_size]
+training_markings = Y[0:training_size]
+end_training = (training_size + test_size)
 
-# Training
+test_data = X[training_size:end_training]
+test_markings = Y[training_size:end_training]
+end_test = training_size + test_size
+
+validation_data = X[end_test:]
+validation_markings = Y[end_test:]
+
+print ("Total training elements: %d" % len(training_data))
+print ("Total test elements: %d" % len(test_data))
+print ("Total validation elements: %d" % len(validation_data))
+
+def fit_and_predict(name, model, training_data, training_markings, test_data, test_markings):
+    model.fit(training_data, training_markings)
+
+    # Evaluating algorithm efficiency
+    result = model.predict(test_data)
+    hits = (result == test_markings)
+    total_hits = sum(hits)
+    total_elements = len(test_data)
+    hit_rate = 100.0 * total_hits / total_elements
+    print ("Algorithm test hit rate: {0}: {1}".format(name,  hit_rate))
+    return hit_rate
+
+
+
+# Training and testing with MultinomialNB
 from sklearn.naive_bayes import MultinomialNB
-model = MultinomialNB()
-model.fit(training_data, training_markings)
+multinomial_model = MultinomialNB()
+multinomial_result = fit_and_predict("MultinomialNB", multinomial_model, training_data, training_markings, test_data, test_markings)
 
-# Evaluating algorithm efficiency
-result = model.predict(test_data)
-hits = (result == test_markings)
+# Training and testing with AdaBoostClassifier
+from sklearn.ensemble import AdaBoostClassifier
+adaboost_model = AdaBoostClassifier()
+adaboost_result = fit_and_predict("AdaBoostClassifier", adaboost_model, training_data, training_markings, test_data, test_markings)
+
+# Choosing winner
+if multinomial_result > adaboost_result:
+    winner_model = multinomial_model
+else:
+    winner_model = adaboost_model
+
+# Validating the winner
+result = winner_model.predict(validation_data)
+hits = (result == validation_markings)
 total_hits = sum(hits)
-total_elements = len(test_data)
+total_elements = len(validation_data)
 hit_rate = 100.0 * total_hits / total_elements
-print ("Algorithm hit rate: %f" % hit_rate)
-print ("Total test elements: %d" % total_elements)
+print ("Winner algorithm validation hit rate: %f" % hit_rate)
 
 # Evaluating dumb(0 or 1) algorithm efficiency
-base_hits = max(Counter(test_markings).itervalues())
-base_hit_rate = 100.0 * base_hits / len(test_markings)
-print ("Base hit rate: %f" % base_hit_rate)
+base_hits = max(Counter(validation_markings).itervalues())
+base_hit_rate = 100.0 * base_hits / len(validation_markings)
+print ("Base test hit rate: %f" % base_hit_rate)
